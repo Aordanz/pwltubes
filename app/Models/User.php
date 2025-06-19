d238187ce05e5ca6fbe8a7cc89f648089005ea71
Aordanz <jordanamadeoh@gmail.com>
2025-06-19 17:21:56 +0700

    masih error

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,29 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-    use HasFactory, Notifiable;
+    use Notifiable;
 
     protected $fillable = [
         'name',
         'email',
         'password',
-        'age_category',
-        'jenis_kelamin',
     ];
 
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        return true; // Ganti dengan pengecekan role jika perlu
+    }
 }
